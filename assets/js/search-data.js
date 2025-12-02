38ba772102cc01b534654c8869f7ae473fddc3ae
wjk5117 <57288491+wjk5117@users.noreply.github.com>
2025-12-02 08:54:14 +0000

    Deploying to gh-pages from @ wjk5117/wjk5117.github.io@069074a1b3b8598f2db7ecff7dd57a7df8e3dfd4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -105,30 +105,4 @@ ninja.data = [{
         handler: () => {
           window.open("https://scholar.google.com/citations?user=L90n6MUAAAAJ", "_blank");
         },
-      },{
-      id: 'light-theme',
-      title: 'Change theme to light',
-      description: 'Change the theme of the site to Light',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("light");
-      },
-    },
-    {
-      id: 'dark-theme',
-      title: 'Change theme to dark',
-      description: 'Change the theme of the site to Dark',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("dark");
-      },
-    },
-    {
-      id: 'system-theme',
-      title: 'Use system default theme',
-      description: 'Change the theme of the site to System Default',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("system");
-      },
-    },];
+      },];
